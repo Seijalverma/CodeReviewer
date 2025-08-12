ce715d6caa35b1d59283f65097dd88618dd7df77
Seijalverma <seijalverma08@gmail.com>
2025-08-12 17:37:58 +0530

    adding backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   }, []);
   async function reviewCode() {
     try {
-      const response = await axios.post('http://localhost:3000/ai/get-review', { code });
+      const response = await axios.post('https://code-reviewer-puce.vercel.app', { code });
       setReview(response.data);
     }
     catch (e) {
