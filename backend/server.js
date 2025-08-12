59e0382e03932f674e0816ef9fd58ea31e5a2659
Seijalverma <seijalverma08@gmail.com>
2025-08-12 17:35:28 +0530

    fixing backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-
-app.listen(3000,()=>{
-    console.log("listening on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
 })
